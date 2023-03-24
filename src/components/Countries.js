3d40f8a95758731f241e49ae7d3b8b5639e6275b
William Baele <baelewilliam1999@gmail.com>
2023-03-24 14:37:00 +0100

    Added axios

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -14,7 +14,18 @@ const Countries = () => {
       <h1 className="text-center text-3xl font-medium mb-10">Countries</h1>
       <div className="grid grid-cols-5 gap-4">
           {
-            data.map((country) => <div className="col-span-1 bg-red-200 p-4">{country.name.common}</div> )
+            data.map((country) =>
+            <div className="col-span-1 bg-red-200 p-4 grid">
+              <h2 className="font-medium text-lg">
+                {country.name.common}
+              </h2>
+              <p>
+                {country.capital}
+              </p>
+              <p>
+                {country.population}
+              </p>
+            </div> )
           }
       </div>
     </div>
